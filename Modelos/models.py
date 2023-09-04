05ac4fddcdbc48213bacb0fc6b87197d7dd1fade
keniavp <you@example.com>
2023-09-04 16:50:55 -0400

    a

--- Modelos/models.py
+++ Modelos/models.py
@@ -1,3 +1,67 @@
 from django.db import models
 
-# Pon aqui todos los modelos
+
+class TipoEvento(models.Model):
+    tipo = models.CharField(max_length=250, verbose_name="Tipo de Evento")
+
+    def __str__(self):
+        return self.tipo
+
+    class Meta:
+        verbose_name = 'tipo_evento'
+        verbose_name_plural = 'tipo_eventos'
+        db_table = 'tipo_evento'
+        
+class Pais(models.Model):
+    pais = models.CharField(max_length=250, verbose_name="País")
+    siglas = models.CharField(max_length=250, verbose_name="Siglas")
+
+    def __str__(self):
+        return self.pais
+    
+    def __str__(self):
+        return self.siglas
+
+    class Meta:
+        verbose_name = 'pais'
+        verbose_name_plural = 'paises'
+        db_table = 'pais'
+        
+class Reglamento(models.Model):
+    tipo = models.CharField(max_length=255, verbose_name="Reglamento", unique=True)
+    cant_r = models.IntegerField(verbose_name="Cantidad de round")
+    duracion = models.IntegerField(verbose_name="Duración")
+
+    def __str__(self):
+        return self.tipo
+    
+    def __str__(self):
+        """
+        Devuelve la cantidad de rounds
+        """
+        return self.cant_r
+    
+    def __str__(self):
+        """
+        Devuelve la duración de los rounds en segundos
+        """
+        return self.duracion
+
+    class Meta:
+        verbose_name = 'reglamento'
+        verbose_name_plural = 'reglamentos'
+        db_table = 'reglamento'
+        
+class Evento(models.Model):
+    nombre = models.CharField(max_length=255, verbose_name="Evento", unique=True)
+    pais = models.ForeignKey(Pais, on_delete=models.SET_NULL, null=True, blank=True)
+    reglamento = models.ForeignKey(Reglamento, on_delete=models.SET_NULL, null=True, blank=True)
+    tipoevento = models.ForeignKey(TipoEvento, on_delete=models.SET_NULL, null=True, blank=True)
+            
+    def __str__(self):
+        return self.nombre    
+   
+    class Meta:
+        verbose_name = 'evento'
+        verbose_name_plural = 'eventos'
+        db_table = 'evento'
--- compuboxcu/settings.py
+++ compuboxcu/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
-        'PORT': '5433',
+        'PORT': '5432',
     }
 }
 
