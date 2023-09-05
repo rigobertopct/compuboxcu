c8585a6867daef72c2337a1a334c036a339f1751
Rigo <rigosofttech@gmail.com>
2023-09-05 12:53:07 -0400

    migraciones

--- Modelos/mutations.py
+++ Modelos/mutations.py
@@ -1,5 +1,45 @@
 import graphene
+from graphene import Mutation
+
+from .models import Pais
+
+
+class NuevoPais(Mutation):
+    class Arguments:
+        pais = graphene.String(required=True)
+        siglas = graphene.String(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, pais, siglas):
+        try:
+            Pais.objects.create(pais=pais, siglas=siglas)
+            return NuevoPais(success=True, errors=None)
+        except Exception as e:
+            return NuevoPais(success=False, errors=str(e))
+
+
+class ActualizarPais(Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        pais = graphene.String(required=True)
+        siglas = graphene.String(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, pais, siglas, id):
+        try:
+            paiss = Pais.objects.get(id=id)
+            paiss.pais = pais
+            paiss.siglas = siglas
+            paiss.save()
+            return ActualizarPais(success=True, errors=None)
+        except Exception as e:
+            return ActualizarPais(success=False, errors=str(e))
 
 
 class Mutation(graphene.ObjectType):
-    pass
+    nuevoPais = NuevoPais.Field()
+    actualizarPais = ActualizarPais.Field()
