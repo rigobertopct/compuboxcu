cf6928e2d0c6d81bfacbd215fd27201bddfecf4b
Rigo <rigosofttech@gmail.com>
2023-09-05 14:03:42 -0400

    migraciones

--- Modelos/mutations.py
+++ Modelos/mutations.py
@@ -1,7 +1,7 @@
 import graphene
 from graphene import Mutation
 
-from .models import Pais
+from .models import *
 
 
 class NuevoPais(Mutation):
@@ -40,6 +40,182 @@ class ActualizarPais(Mutation):
             return ActualizarPais(success=False, errors=str(e))
 
 
+class EliminarPais(Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, id):
+        try:
+            paiss = Pais.objects.get(id=id)
+            paiss.delete()
+            return EliminarPais(success=True, errors=None)
+        except Exception as e:
+            return EliminarPais(success=False, errors=str(e))
+
+
+class NuevoCodifResultado(Mutation):
+    class Arguments:
+        resultado = graphene.String(required=True)
+        descripcion = graphene.String(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, resultado, descripcion):
+        try:
+            CodifResultado.objects.create(resul=resultado, descripcion=descripcion)
+            return NuevoCodifResultado(success=True, errors=None)
+        except Exception as e:
+            return NuevoCodifResultado(success=False, errors=str(e))
+
+
+class ActualizarCodifResultado(Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        resultado = graphene.String(required=True)
+        descripcion = graphene.String(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, resultado, id, descripcion):
+        try:
+            item = CodifResultado.objects.get(id=id)
+            item.resul = resultado
+            item.descripcion = descripcion
+            item.save()
+            return ActualizarCodifResultado(success=True, errors=None)
+        except Exception as e:
+            return ActualizarCodifResultado(success=False, errors=str(e))
+
+
+class EliminarCodifResultado(Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, id):
+        try:
+            item = CodifResultado.objects.get(id=id)
+            item.delete()
+            return EliminarCodifResultado(success=True, errors=None)
+        except Exception as e:
+            return EliminarCodifResultado(success=False, errors=str(e))
+
+
+class NuevoTipoEvento(Mutation):
+    class Arguments:
+        nombre = graphene.String(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, nombre):
+        try:
+            TipoEvento.objects.create(tipo=nombre)
+            return NuevoTipoEvento(success=True, errors=None)
+        except Exception as e:
+            return NuevoTipoEvento(success=False, errors=str(e))
+
+
+class ActualizarTipoEvento(Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        nombre = graphene.String(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, nombre, id):
+        try:
+            item = TipoEvento.objects.get(id=id)
+            item.tipo = nombre
+            item.save()
+            return ActualizarTipoEvento(success=True, errors=None)
+        except Exception as e:
+            return ActualizarTipoEvento(success=False, errors=str(e))
+
+
+class EliminarTipoEvento(Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, id):
+        try:
+            item = TipoEvento.objects.get(id=id)
+            item.delete()
+            return EliminarTipoEvento(success=True, errors=None)
+        except Exception as e:
+            return EliminarTipoEvento(success=False, errors=str(e))
+
+
+class NuevoReglamento(Mutation):
+    class Arguments:
+        pais = graphene.String(required=True)
+        siglas = graphene.String(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, pais, siglas):
+        try:
+            Pais.objects.create(pais=pais, siglas=siglas)
+            return NuevoPais(success=True, errors=None)
+        except Exception as e:
+            return NuevoPais(success=False, errors=str(e))
+
+
+class ActualizarReglamento(Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+        pais = graphene.String(required=True)
+        siglas = graphene.String(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, pais, siglas, id):
+        try:
+            paiss = Pais.objects.get(id=id)
+            paiss.pais = pais
+            paiss.siglas = siglas
+            paiss.save()
+            return ActualizarPais(success=True, errors=None)
+        except Exception as e:
+            return ActualizarPais(success=False, errors=str(e))
+
+
+class EliminarReglamento(Mutation):
+    class Arguments:
+        id = graphene.Int(required=True)
+
+    success = graphene.Boolean()
+    errors = graphene.String()
+
+    def mutate(self, info, id):
+        try:
+            paiss = Pais.objects.get(id=id)
+            paiss.delete()
+            return EliminarPais(success=True, errors=None)
+        except Exception as e:
+            return EliminarPais(success=False, errors=str(e))
+
+
 class Mutation(graphene.ObjectType):
     nuevoPais = NuevoPais.Field()
     actualizarPais = ActualizarPais.Field()
+    eliminarPais = EliminarPais.Field()
+    nuevoCodResultado = NuevoCodifResultado.Field()
+    actualizarCodResultado = ActualizarCodifResultado.Field()
+    eliminarCodResultado = EliminarCodifResultado.Field()
+    nuevoTipoEvento = NuevoTipoEvento.Field()
+    actualizarTipoEvento = ActualizarTipoEvento.Field()
+    eliminarTipoEvento = EliminarTipoEvento.Field()
